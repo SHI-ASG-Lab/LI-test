87b5475989143eaf5186dd7096ff5b78ba802c7f
j-isley <79328510+j-isley@users.noreply.github.com>
2022-02-17 09:02:33 -0600

    Add files via upload

--- test.tf
+++ test.tf
@@ -1,60 +1,28 @@
 # Variable Declarations
 
-variable "instance_count" {
-    type    = number
-    default = 5
-}
-variable "additional_volume_size" {
+variable "volume_size" {
     type    = number
     default = 20
 }
-variable "volume_count" {
-    type    = number
-    default = 3
+
+variable "zones" {
+    type = list(string)
+    default = ["us-west-1b", "us-west-1c"]
 }
-# Declare the data source
-data "aws_availability_zones" "available" {
-    all_availability_zones = true
-    state                  = "available"
-}
+
+# Make a set of available AZs
 
 locals {
-    az = toset(data.aws_availability_zones.available.names)
+    az = toset(var.zones)
 }
 
-# Resource Code Block
-
-# Size value changed
-/*
-resource "aws_ebs_volume" "ebs_volume" {
-    count             = length[data.aws_availability_zones.available.names]
-    availability_zone = aws_instance.sql_server.availability_zone.name
-    size              = var.additional_volume_size
-}
-*/
+# Create one Volume per AZ
 
 resource "aws_ebs_volume" "ebs_volume" {
     for_each          = local.az
     availability_zone = each.value
-    size              = var.additional_volume_size
+    size              = var.volume_size
 }
-
-/*
-resource "aws_ebs_volume" "ebs_volume" {
-    count             = var.instance_count * var.volume_count
-    availability_zone = aws_instance.sql_server[floor(count.index / var.volume_count)].availability_zone
-    size              = var.additional_volume_size[count.index % var.volume_count]
-}
-*/
-
-/*
-resource "aws_volume_attachment" "volume_attachment" {
-    count       = var.instance_count * var.volume_count
-    volume_id   = element(aws_ebs_volume.ebs_volume.*.id, count.index)
-    device_name = element(var.ec2_volumes, count.index)
-    instance_id = element(aws_instance.sql_server.*.id, floor(count.index / var.volume_count))
-}
-*/
 
 # Instance Creation
 
